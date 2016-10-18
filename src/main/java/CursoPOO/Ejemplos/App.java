14c3b7aabcb7fe1cc6bf9146d8984a41efe98a64
Sara <saara3011@gmail.com>
2016-10-18 09:53:02 +0200

    Creación del proyecto

---
+++ src/main/java/CursoPOO/Ejemplos/App.java
@@ -0,0 +1,13 @@
+package CursoPOO.Ejemplos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
